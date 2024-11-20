fa18cee2cc1078f45ba46c815e48e018c3ca560e
Amr <msam.team20@gmail.com>
2024-11-20 19:07:14 +0200

    Solve Should

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ import { connect } from 'mongoose';
 import config from '../src/libs/configuration.js';
 
 const dbConnect = async () => {
-  await connect('mongodb+srv://amrheshammacc:medicadb@medica.s1d3y.mongodb.net/?retryWrites=true&w=majority&appName=medica')
+  await connect(process.env.MONGO_URI)
     .then(() => console.log('Database connection established'))
     .catch((err) => console.log('Error connecting to Database: ', err));
 };
