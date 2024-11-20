a91d9ee0e602123297f68123d2ba2051fa0e76b2
Amr <msam.team20@gmail.com>
2024-11-20 19:11:57 +0200

    SubCategories

--- src/modules/subCategories/subCategories.routes.js
+++ src/modules/subCategories/subCategories.routes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import * as subCategoriesController from './subCategories.controller.js';
 import { upload } from '../../middlewares/multer.js';
+import preInitializeSession from '../../middlewares/preInitializeSession.js';
 
 const router = Router();
 
-router.get('/sub-categories', subCategoriesController.getSubCategories);
-router.get('/sub-category/:id', subCategoriesController.getSubCategory);
+router.get('/sub-categories', preInitializeSession, subCategoriesController.getSubCategories);
+router.get('/sub-category/:id', preInitializeSession, subCategoriesController.getSubCategory);
 router.post('/sub-category', upload.single('image'), subCategoriesController.addSubCategory);
 router.patch('/sub-category/:id', subCategoriesController.updateSubCategory);
 router.delete('/sub-category/:id', subCategoriesController.deleteSubCategory);
