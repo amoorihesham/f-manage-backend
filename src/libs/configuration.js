0320ad8a5c5cb66e58bfdca48014a25d01853b41
Amr <msam.team20@gmail.com>
2024-11-20 18:06:34 +0200

    Live DB

--- src/libs/configuration.js
+++ src/libs/configuration.js
@@ -1,6 +1,6 @@
 const config = {
   jwtSecret: process.env.JWT_SECRET,
-  dbURL: process.env.NODE_ENV === 'production' ? process.env.MONGO_URI : 'mongodb://localhost:27017/fmanage',
+  dbURL: process.env.MONGO_URI,
   dbConnection: process.env.NODE_ENV === 'production' ? 'Mongo Atlas' : 'Local Connection',
   crossOrigin: 'http://localhost:5173',
   port: process.env.PORT || 3000,
